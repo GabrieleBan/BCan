b0f1e6170e883f00a1a7c168f19b6f1750238ced
Gabriele Banchio <gabriele.banchio2001@gmail.com>
2026-05-22 15:41:22 +0200

    data can be loaded and returned after scaling

--- dataloader.py
+++ dataloader.py
@@ -1,6 +1,35 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import pandas as pd
 
+def __get_data():
+    dataset= load_breast_cancer(as_frame=True)
+    return dataset
 
-def get_data():
+def get_data_scaled():
     dataset= load_breast_cancer()
-    return dataset.data,dataset.target
+    scaler=StandardScaler()
+    data=dataset.data
+    data=scaler.fit_transform(data)
+    print(data[:,0].std())
+    return data,dataset.target
+
+
+
+def test():
+     
+    data,t=get_data_scaled()
+
+    print(data)
+    print(t)
+
+
+
+
+    
+
+    
+    
+if __name__ == '__main__':
+    test()
